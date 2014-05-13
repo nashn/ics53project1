d63d62a8e8c4e3f4baa09b7e0805b42baa9eb801
nashn <nash@dhcp-v033-245.mobile.uci.edu>
2014-05-12 19:50:07 -0700

    Add main to project 2

---
+++ ics53_project_2/main.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : First Project Lab
+//============================================================================
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+	
+
+	return 0;
+}
