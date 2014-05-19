1ece18455053c00099198c515f40ef03948112f1
nashn <nies@uci.edu>
2014-05-19 14:10:44 -0700

    Add new files

--- ics53_project_2/main.cpp
+++ ics53_project_2/main.cpp
@@ -7,10 +7,55 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
 
-int main(int argc, char* argv[]) {
+#include <pthread.h>
 
-	
+using namespace std;
+
+void simulation(string& filename);
+
+int main(int argc, char* argv[])
+{
+	string line = "";
+
+	getline(cin, line);
+
+
+	while( 1 )
+	{
+		if ( line == "quit" )
+			break;
+		else
+		{
+			simulation(line);
+			break;
+		}
+	}
+
 
 	return 0;
+}
+
+void simulation(string& filename)
+{
+	ifstream f(filename.c_str());
+	string line = "";
+
+	int i = 0;
+
+	if ( f.is_open() )
+	{
+		while ( getline (f,line) )
+		{
+			cout << "The " << i << " line contains: " << line << endl;
+			i++;
+		}
+		f.close();
+	}
+	else cout << "Unable to open file" << endl;
+
+
 }
